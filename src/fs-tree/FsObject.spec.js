7212cd08c27f0d8fb893e9c51903fbcf8882d77f
Rasmus Ljungmann Pedersen <rasmuslp@gmail.com>
2018-10-04 17:57:48 +0200

    test(jest): Added jest as test framework

---
+++ src/fs-tree/FsObject.spec.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+const FsObject = require('./FsObject');
+
+describe('FsObject', () => {
+	const pathToFsObject = path.join(__dirname, 'FsObject.js');
+	const stats = {
+		size: 0
+	};
+
+	let fsObject;
+	beforeEach(() => {
+		fsObject = new FsObject(pathToFsObject, stats);
+	});
+
+	test('path', () => {
+		expect(fsObject.path).toBe(pathToFsObject);
+	});
+
+	test('size', () => {
+		expect(fsObject.size).toBe(0);
+	});
+});
