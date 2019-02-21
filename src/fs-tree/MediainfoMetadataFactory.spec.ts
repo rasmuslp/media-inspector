b4de0663cf83f5201f53ffe121107d1bda25d475
Rasmus Ljungmann Pedersen <rasmuslp@gmail.com>
2019-02-21 20:23:21 +0100

    test(MediainfoMetadataFactory): Replaced test clip with smaller instance

--- src/fs-tree/MediainfoMetadataFactory.spec.ts
+++ src/fs-tree/MediainfoMetadataFactory.spec.ts
@@ -1,9 +1,9 @@
 import { MediainfoMetadataFactory } from './MediainfoMetadataFactory';
 
-import mediainfoOutput from '../../test-assets/SampleVideo_1280x720_1mb.mediainfo';
+import mediainfoOutput from '../../test-assets/SampleVideo.mediainfo';
 
 describe('MediainfoMetadataFactory', () => {
-	const partialPath = 'test-assets/SampleVideo_1280x720_1mb.mp4';
+	const partialPath = 'test-assets/SampleVideo.mov';
 
 	test(`can _readFromFile '${partialPath}'`, async () => {
 		const metadata = await MediainfoMetadataFactory._readFromFileSystem(partialPath);
