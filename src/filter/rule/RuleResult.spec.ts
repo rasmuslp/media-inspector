99bf23fb809754351c6c829294ef96387aeb9736
Rasmus Ljungmann Pedersen <rasmuslp@gmail.com>
2019-11-22 22:01:11 +0100

    chore(Package): Upgraded tooling

--- src/filter/rule/RuleResult.spec.ts
+++ src/filter/rule/RuleResult.spec.ts
@@ -38,9 +38,42 @@ describe('#satisfied', () => {
 	});
 });
 
-describe('one failed on satisfied', () => {
-	let results;
-	beforeEach(() => {
+describe('#getResultsAsStrings', () => {
+	test('that string reports as satisfied for passed checks', () => {
+		const condition = ConditionFactory.getFromSerialized({
+			path: 'video.framerate',
+			operator: ConditionOperator.GREATER_THAN_OR_EQUAL,
+			value: 25
+		});
+		const result = condition.check(25);
+
+		const ruleResult = new RuleResult([result]);
+		const resultAsStrings = ruleResult.getResultsAsStrings();
+
+		// Test
+		expect(resultAsStrings).toHaveLength(1);
+		expect(resultAsStrings[0]).toMatch(/satisfied/);
+	});
+
+	test('that string reports as failed for failed checks', () => {
+		const condition = ConditionFactory.getFromSerialized({
+			path: 'audio.channels',
+			operator: ConditionOperator.GREATER_THAN_OR_EQUAL,
+			value: 2
+		});
+		const result = condition.check(1);
+
+		const ruleResult = new RuleResult([result]);
+		const resultAsStrings = ruleResult.getResultsAsStrings();
+
+		// Test
+		expect(resultAsStrings).toHaveLength(1);
+		expect(resultAsStrings[0]).toMatch(/failed/);
+	});
+});
+
+describe('#getWeightedScore', () => {
+	test('#1: Fail, #2: Pass --> 1', () => {
 		const conditions = [
 			ConditionFactory.getFromSerialized({
 				path: 'video.framerate',
@@ -55,31 +88,36 @@ describe('one failed on satisfied', () => {
 			})
 		];
 
-		results = [
-			// Should pass
-			conditions[0].check(25),
+		const results = [
+			conditions[0].check(23),
+			conditions[1].check(2)
+		];
 
-			// Should fail
+		const ruleResult = new RuleResult(results);
+		const score = ruleResult.getWeightedScore();
+		expect(score).toBe(1);
+	});
+
+	test('#1: Pass, #2: Fail --> 4', () => {
+		const conditions = [
+			ConditionFactory.getFromSerialized({
+				path: 'video.framerate',
+				operator: ConditionOperator.GREATER_THAN_OR_EQUAL,
+				value: 25
+			}),
+
+			ConditionFactory.getFromSerialized({
+				path: 'audio.channels',
+				operator: ConditionOperator.GREATER_THAN_OR_EQUAL,
+				value: 2
+			})
+		];
+
+		const results = [
+			conditions[0].check(25),
 			conditions[1].check(1)
 		];
 
-		// NB: Is it bad practice to check the test input here?
-		// Check input
-		expect(results[0].satisfied).toBe(true);
-		expect(results[1].satisfied).toBe(false);
-	});
-
-	test('#getResultsAsStrings', () => {
-		const ruleResult = new RuleResult(results);
-		const resultAsStrings = ruleResult.getResultsAsStrings();
-
-		// Test
-		expect(resultAsStrings).toHaveLength(2);
-		expect(resultAsStrings[0]).toMatch(/satisfied/);
-		expect(resultAsStrings[1]).toMatch(/failed/);
-	});
-
-	test('#getWeightedScore', () => {
 		const ruleResult = new RuleResult(results);
 		const score = ruleResult.getWeightedScore();
 		expect(score).toBe(4);
