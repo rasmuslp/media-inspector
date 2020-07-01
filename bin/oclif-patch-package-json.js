231494dced3a807460e893eb2144e83017c34140
Rasmus Ljungmann Pedersen <rasmuslp@gmail.com>
2020-07-01 21:08:56 +0200

    ci(release): Added patch for changing path to commands before bundling for publishing

---
+++ bin/oclif-patch-package-json.js
@@ -0,0 +1,30 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const fs = require('fs');
+const path = require('path');
+const { promisify } = require('util');
+
+const readFile = promisify(fs.readFile);
+const writeFile = promisify(fs.writeFile);
+
+async function run() {
+	const pJsonPath = path.resolve(__dirname, '..', 'package.json');
+
+	const content = await readFile(pJsonPath, 'utf8');
+
+	const searchPattern = /"commands": "\.\/src\/cli\/commands"/;
+	const replacePattern = '"commands": "./dist/cli/commands"';
+
+	if (!searchPattern.test(content)) {
+		console.log(`Search pattern (${searchPattern.toString()}) not found!`);
+		// eslint-disable-next-line no-process-exit,unicorn/no-process-exit
+		process.exit(-1);
+	}
+
+	const patchedContent = content.replace(searchPattern, replacePattern);
+
+	await writeFile(pJsonPath, patchedContent);
+
+	console.log('OK');
+}
+
+void run();
