9c878aad5abaeae9c711ab87b1a34b6b7ab8ff81
Rasmus Ljungmann Pedersen <rasmuslp@gmail.com>
2020-07-02 20:52:05 +0200

    test: Changed to collect coverage from all src/ code

--- jest.config.js
+++ jest.config.js
@@ -22,15 +22,18 @@ module.exports = {
 	// collectCoverage: false,
 
 	// An array of glob patterns indicating a set of files for which coverage information should be collected
-	// collectCoverageFrom: null,
+	collectCoverageFrom: [
+		'src/**/*.{js,ts}'
+	],
 
 	// The directory where Jest should output its coverage files
 	// coverageDirectory: null,
 
 	// An array of regexp pattern strings used to skip coverage collection
-	// coveragePathIgnorePatterns: [
-	//   '/node_modules/'
-	// ],
+	coveragePathIgnorePatterns: [
+		'/node_modules/',
+		'spec\\.'
+	],
 
 	// A list of reporter names that Jest uses when writing coverage reports
 	// coverageReporters: [
