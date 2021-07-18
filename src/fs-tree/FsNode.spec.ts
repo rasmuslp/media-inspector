98d65af7cd795b93bfae4050567e24805a853a47
Rasmus Ljungmann Pedersen <rasmuslp@gmail.com>
2021-07-19 00:13:12 +0200

    test(FsNode): Remove unused implementation of removed abstract methods

--- src/fs-tree/FsNode.spec.ts
+++ src/fs-tree/FsNode.spec.ts
@@ -2,32 +2,24 @@ import path from 'path';
 
 import { FsNode } from './FsNode';
 
-class FsNodeImpl extends FsNode {
-	isDirectory(): boolean {
-		throw new Error('Method not implemented.');
-	}
-
-	isFile(): boolean {
-		throw new Error('Method not implemented.');
-	}
-}
+class FsNodeImpl extends FsNode {}
 
 describe('FsNode', () => {
-	const pathTofsNode = path.join(__dirname, 'FsNode.js');
+	const pathToFsNode = path.join(__dirname, 'FsNode.js');
 	const stats = {
-		size: 0
+		size: 123
 	};
 
 	let fsNode: FsNode;
 	beforeEach(() => {
-		fsNode = new FsNodeImpl(pathTofsNode, stats);
+		fsNode = new FsNodeImpl(pathToFsNode, stats);
 	});
 
 	test('path', () => {
-		expect(fsNode.path).toBe(pathTofsNode);
+		expect(fsNode.path).toBe(pathToFsNode);
 	});
 
 	test('size', () => {
-		expect(fsNode.size).toBe(0);
+		expect(fsNode.size).toBe(123);
 	});
 });
