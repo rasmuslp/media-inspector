8c76b6f17e70a9f628b091216dde153b6538007d
Rasmus Ljungmann Pedersen <rasmuslp@gmail.com>
2021-07-22 22:43:24 +0200

    refactor(metadata): Add MetadataCacheFactory for deserialization

--- src/cli/glue/readMetadataFromSerialized.ts
+++ src/cli/glue/readMetadataFromSerialized.ts
@@ -1,9 +1,7 @@
 import cli from 'cli-ux';
 
-import { FsTreeFactory } from '../../fs-tree';
-import { MetadataCache, MetadataCacheSchema } from '../../metadata/MetadataCache';
-import { MediainfoMetadata } from '../../metadata/mediainfo/MediainfoMetadata';
-import { MediainfoMetadataFactory } from '../../metadata/mediainfo/MediainfoMetadataFactory';
+import { MetadataCache } from '../../metadata/MetadataCache';
+import { MetadataCacheFactory } from '../../metadata/MetadataCacheFactory';
 import { SerializableIO } from '../../serializable/SerializableIO';
 
 export async function readMetadataFromSerialized(path: string, verbose = false): Promise<MetadataCache> {
@@ -12,19 +10,7 @@ export async function readMetadataFromSerialized(path: string, verbose = false):
 	}
 
 	const serialized = await SerializableIO.read(path);
-	if (serialized.type !== 'MetadataCache') {
-		throw new Error(`readMetadataFromSerialized cannot read ${serialized.type}`);
-	}
-	const parsed = MetadataCacheSchema.parse(serialized.data);
-	const fsTree = FsTreeFactory.getTreeFromSerialized(parsed.tree);
-	const videoMetadata = new Map<string, MediainfoMetadata>();
-
-	for (const [path, data] of Object.entries(parsed.metadata)) {
-		const metadata = MediainfoMetadataFactory.getFromSerialized(data);
-		videoMetadata.set(path, metadata);
-	}
-
-	const metadataCache = new MetadataCache(fsTree, videoMetadata);
+	const metadataCache = MetadataCacheFactory.getFromSerialized(serialized);
 
 	if (verbose) {
 		cli.action.stop();
